7d4566ea919c68f7def5cc2a46545ec07194ee58
Christian Flor <christian.flor1@correo.icesi.edu.co>
2020-02-08 13:15:32 -0500

    Tried ineffectively to implement bubble sort in C#. It is not yet working

--- model/Sorter.cs
+++ model/Sorter.cs
@@ -13,6 +13,15 @@ namespace model
         public static void BubleSort<T>(List<T> items) where T : IComparable
         { 
             
+            for (int i = 0; i < items.Count; i++) {
+                for (int j = 0; j < items.Count - 1; j++) {
+                     if (arr[j] > items[j + 1]) {
+                        int temp = items[j + 1];
+                        items[j + 1] = items[j];
+                        items[j] = temp;
+                     }
+                }
+            }
         }
 
         public static void InsertionSort<T>(List<T> items) where T : IComparable
